09454ea0fd331c8ad18e98718eccca8294634b7c
Jonathan Molina <Jon.molina@duocuc.cl>
2024-09-24 22:25:54 -0300

    inicio vista profe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'vista-profe',
+    loadChildren: () => import('./pages/vista-profe/vista-profe.module').then( m => m.VistaProfePageModule)
   },
+
 ];
 
 @NgModule({
---
+++ src/app/pages/vista-profe/vista-profe.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-profe',
+  templateUrl: './vista-profe.page.html',
+  styleUrls: ['./vista-profe.page.scss'],
+})
+export class VistaProfePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
