29b1510c05c20b1600866a1c822cfce0e206d5ed
Jonathan Molina <Jon.molina@duocuc.cl>
2024-09-25 23:23:10 -0300

    actualizacion lista

--- src/app/pages/lista-pres/lista-pres.page.ts
+++ src/app/pages/lista-pres/lista-pres.page.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPresPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router) { }
+al_vistaprofe(){
+  this.router.navigate(["/codigoprofe"])
+}
   ngOnInit() {
   }
 
