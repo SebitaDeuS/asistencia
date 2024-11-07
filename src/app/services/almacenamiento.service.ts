7e61ee4b9cc4b13128bc1e6efff1b80051af5294
Jonathan Molina <Jon.molina@duocuc.cl>
2024-11-07 18:51:19 -0300

    mostrar cargar

--- src/app/services/almacenamiento.service.ts
+++ src/app/services/almacenamiento.service.ts
@@ -2,6 +2,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,34 +11,36 @@ export class AuthService {
   constructor(
     private afAuth: AngularFireAuth,
     private firestore: AngularFirestore,
+    private utilsSvc:UtilsService,
   ) {}
 
   async registerUser(email: string, password: string) {
     try {
       const userCredential = await this.afAuth.createUserWithEmailAndPassword(email, password);
       console.log('Se guarda en auth')
-      // await this.saveUserDataToFirestore(userCredential.user?.uid, email, password);
       console.log('se guarda en firestore?')
       return userCredential;
     } catch (error) {
       console.error('Error en el registro:', error);
       throw error;
+    }finally{
+      await this.utilsSvc.hideLoading();
     }
   }
 
   public async saveUserDataToFirestore(userId: string | undefined, email: string, password: string) {
     if (userId) {
       const docRef = this.firestore
-        .collection('cursos')  // Cambia si "cursos" es el nombre correcto de la colección principal
-        .doc('FnU6lE1vSejqvw90bKUd') // ID del documento del curso específico
-        .collection('secciones')  // Subcolección "secciones"
-        .doc('001');// Documento específico de la sección
+        .collection('cursos')  
+        .doc('FnU6lE1vSejqvw90bKUd') 
+        .collection('secciones')  
+        .doc('001');
         
-      // Actualizamos el campo "alumnos" usando arrayUnion para agregar un nuevo objeto alumno.
       try {
+        await this.utilsSvc.showLoading();
         const docSnapshot = await docRef.get().toPromise();
         if (docSnapshot.exists) {
-          // Si el documento existe, usa update para agregar el alumno
+         
           await docRef.update({
             alumnos: firebase.firestore.FieldValue.arrayUnion({
               id_alumno: userId,
@@ -46,7 +49,6 @@ export class AuthService {
             })
           });
         } else {
-          // Si el documento no existe, usa set para crear el documento con el array de alumnos
           await docRef.set({
             alumnos: [{
               id_alumno: userId,
@@ -58,6 +60,8 @@ export class AuthService {
         console.log('Usuario guardado en Firestore');
       } catch (error) {
         console.error('Error al guardar en Firestore:', error);
+      }finally{
+        await this.utilsSvc.hideLoading();
       }
     } else {
       console.log('Error al guardar en Firestore: userId no definido');
