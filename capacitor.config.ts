baca32b7d7c69ec7a7f63e125b1236dcafcce80e
Jonathan Molina <Jon.molina@duocuc.cl>
2024-12-06 13:07:29 -0300

    app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'asistencia',
+  appName: 'Asistencia Duoc',
   webDir: 'www'
 };
 
